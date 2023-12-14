fe6e2ab6fb2956838dc7a3471e7f6188842ed2a6
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-14 19:49:48 +0530

    Explore and Home jsx and scss file added

---
+++ src/Pages/Explore/Explore.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Explore.scss";
+const Explore = () => {
+  return <div></div>;
+};
+
+export default Explore;
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Home.scss";
+const Home = () => {
+  return <div></div>;
+};
+
+export default Home;

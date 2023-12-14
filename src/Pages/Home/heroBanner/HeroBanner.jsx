32852ba89c56033df049570c6fd254e3e58150dd
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-14 21:10:54 +0530

    Add search bar to HeroBanner

--- src/Pages/Home/heroBanner/HeroBanner.jsx
+++ src/Pages/Home/heroBanner/HeroBanner.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./HeroBanner.scss";
 const HeroBanner = () => {
+  const [background, setBackground] = useState("");
+  const [query, setQuery] = useState("");
+  const searchQueryHandler = (event) => {
+    if (event.key === "Enter" && query.length > 0) {
+    }
+  };
+
   return (
     <div className="heroBanner">
       <div className="wrapper">
@@ -9,7 +16,15 @@ const HeroBanner = () => {
           <span className="subTitle">
             Millions of Movies, TV shows and people to discover. Explore now.
           </span>
-          <input type="text" />
+          <div className="searchInput">
+            <input
+              type="text"
+              placeholder="Search for a movie or tvshow..."
+              onChange={(e) => setQuery(e.target.value)}
+              onKeyUp={searchQueryHandler}
+            />
+            <button>Search</button>
+          </div>
         </div>
       </div>
     </div>

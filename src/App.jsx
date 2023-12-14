53b7e9dc0fb1b31f426bac6507e0e4c7682d9f7c
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-14 18:47:12 +0530

    Remove Redux and related code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import { useState, useEffect } from "react";
 import { fetchDataFromApi } from "./utils/api";
-import { useSelector, useDispatch } from "react-redux";
-import { getApiConfiguration } from "./Store/homeSlice";
 
 function App() {
-  const dispatch = useDispatch();
   useEffect(() => {
     apiTesting();
   }, []);
@@ -12,7 +9,6 @@ function App() {
   const apiTesting = () => {
     fetchDataFromApi("/movie/popular").then((res) => {
       console.log(res);
-      dispatch(getApiConfiguration(res));
     });
   };
   return <div className="App"></div>;
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
-import { Store } from "./Store/Store.js";
-import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider Store={Store}>
+  <React.StrictMode>
     <App />
-  </Provider>
+  </React.StrictMode>
 );

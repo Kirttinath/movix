dbda9e8ef64c6fd6b907944e6d31b86e5d8760e3
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-14 20:17:54 +0530

    Add routing to the application.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { fetchDataFromApi } from "./utils/api";
 import { useSelector, useDispatch } from "react-redux";
 import { getApiConfiguration } from "./Store/homeSlice";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import Home from "./Pages/Home/Home";
+import Details from "./Pages/Details/Details";
+import Searchresult from "./Pages/searchResult/SearchResult";
+import Explore from "./Pages/Explore/Explore";
+import pageNotFound from "./Pages/404/pageNotFound";
 
 function App() {
   const dispatch = useDispatch();
@@ -16,7 +24,13 @@ function App() {
       dispatch(getApiConfiguration(res));
     });
   };
-  return <div className="App">{url?.total_pages}</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;

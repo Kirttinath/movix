3da1eabda589542169160a51d47a525dffc7b842
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-14 20:38:51 +0530

    **Removed Header and Footer components from App.js**

--- src/Pages/Home/heroBanner/HeroBanner.jsx
+++ src/Pages/Home/heroBanner/HeroBanner.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 import "./HeroBanner.scss";
 const HeroBanner = () => {
-  return <div>HeroBanner</div>;
+  return (
+    <div className="heroBanner">
+      <div className="wrapper">
+        <div className="heroBannerContent">
+          <span className="title">Welcome</span>
+          <span className="subTitle">
+            Millions of Movies, TV shows and people to discover. Explore now.
+          </span>
+          <input type="text" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HeroBanner;

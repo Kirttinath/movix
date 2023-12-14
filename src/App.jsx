52e7df3864baaf1edaffac419fa9b5863ad79d8c
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-14 18:20:21 +0530

    Add Redux action to dispatch API configuration data

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
 import fetchDataFromApi from "./utils/api";
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration } from "./Store/homeSlice";
 
 function App() {
+  const dispatch = useDispatch();
   useEffect(() => {
     apiTesting();
   }, []);
@@ -9,6 +12,7 @@ function App() {
   const apiTesting = () => {
     fetchDataFromApi("/movie/popular").then((res) => {
       console.log(res);
+      dispatch(getApiConfiguration(res));
     });
   };
   return <div className="App"></div>;

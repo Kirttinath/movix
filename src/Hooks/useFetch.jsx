9a04d5b6314ac6489ba88abd36e651afaafb21b6
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-15 12:49:07 +0530

    Add `useFetch` hook

--- src/Hooks/useFetch.jsx
+++ src/Hooks/useFetch.jsx
@@ -1,2 +1,8 @@
 import { useEffect, useState } from "react";
 import { fetchDataFromApi } from "../utils/api";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(null);
+  const [error, setError] = useState(null);
+};

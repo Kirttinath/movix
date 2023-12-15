a229cf7e10bfcb07944f5ba300d5fb6b0c737f35
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-15 19:00:23 +0530

    Update HeroBanner component to use background image from `background` prop and add an opacity layer.

--- src/Pages/Home/heroBanner/HeroBanner.jsx
+++ src/Pages/Home/heroBanner/HeroBanner.jsx
@@ -28,8 +28,13 @@ const HeroBanner = () => {
 
   return (
     <div className="heroBanner">
-      <div className="backdrop-img"></div>
-      <div className="wrapper">
+      {!loading && (
+        <div className="backdrop-img">
+          <Img src={background} />
+        </div>
+      )}
+      <div className="opacity-layer"></div>
+      <ContentWrapper>
         <div className="heroBannerContent">
           <span className="title">Welcome</span>
           <span className="subTitle">
@@ -45,7 +50,7 @@ const HeroBanner = () => {
             <button>Search</button>
           </div>
         </div>
-      </div>
+      </ContentWrapper>
     </div>
   );
 };

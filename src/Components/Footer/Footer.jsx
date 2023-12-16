9e2a66c4611481170978fe2b3a9f32c1230d384f
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-16 20:36:46 +0530

    Add social media icons and menu items to the footer.

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaTwitter,
+  FaLinkedin,
+} from "react-icons/fa";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
 import "./Footer.scss";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <ContentWrapper>
+        <ul className="menuItems">
+          <li className="menuItem">Terms Of Use</li>
+          <li className="menuItem">Privacy-Policy</li>
+          <li className="menuItem">About</li>
+          <li className="menuItem">Blog</li>
+          <li className="menuItem">FAQ</li>
+        </ul>
+      </ContentWrapper>
+    </footer>
+  );
 };
 
 export default Footer;

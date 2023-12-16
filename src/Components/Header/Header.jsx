055ee6938d43fd9b877bf09bb374d55b2ee870c8
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-16 16:00:07 +0530

    Add search and mobile menu functionality to the header.

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.scss";
+import { HiOutlineSearch } from "react-icons/hi";
+import { SlMenu } from "react-icons/sl";
+import { VscChromeClose } from "react-icons/vsc";
+import { useNavigate, useLocation } from "react-router-dom";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+import logo from "../../assets/movix-logo.svg";
+
 const Header = () => {
-  return <div>Header</div>;
+  const [show, setShow] = useState("top");
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [query, setQuery] = useState("");
+  const [showSearch, setShowSearch] = useState("");
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  return (
+    <header>
+      <ContentWrapper>
+        <div className="logo">
+          <img src={logo} alt="" />
+        </div>
+      </ContentWrapper>
+    </header>
+  );
 };
 
 export default Header;

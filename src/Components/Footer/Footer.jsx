fa0a399dc8869fb5fe03fec1301c2376dd517df3
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-16 20:37:11 +0530

    Add info text to the footer.

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -20,6 +20,14 @@ const Footer = () => {
           <li className="menuItem">Blog</li>
           <li className="menuItem">FAQ</li>
         </ul>
+        <div className="infoText">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.
+        </div>
       </ContentWrapper>
     </footer>
   );

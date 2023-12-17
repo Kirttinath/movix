389f92df0eee59d9b16936f5f9bd29f912e80de7
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-17 15:11:51 +0530

    Add a trending carousel section to the home page.

--- src/Pages/Home/Trending/Trending.jsx
+++ src/Pages/Home/Trending/Trending.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import ContentWrapper from "../../../Components/ContentWrapper/ContentWrapper";
 
 const Trending = () => {
-  return <div>Trending</div>;
+  return (
+    <div className="carouselSection">
+      <ContentWrapper>
+        <span className="carouselTitle">Trending</span>
+      </ContentWrapper>
+    </div>
+  );
 };
 
 export default Trending;

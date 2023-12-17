e389e36ec5f1b3157bcc546ed19c12dd1fdd44ca
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-17 15:05:37 +0530

    Add Trending component to the home page.

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Home.scss";
 import HeroBanner from "./heroBanner/heroBanner";
+import Trending from "./Trending/Trending";
 
 const Home = () => {
   return (
     <div className="homePage">
       <HeroBanner />
+      <Trending />
+      <div style={{ height: 1000 }}></div>
     </div>
   );
 };

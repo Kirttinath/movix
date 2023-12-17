9a92c62d78369be73616ddea0b152b14670d64a8
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-17 23:05:40 +0530

    Added carousel navigation buttons and event handlers.

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -11,7 +11,24 @@ import Img from "../lazyLoadImage/Img";
 import PosterFallback from "../../assets/no-poster.png";
 import "./Carousel.scss";
 const Carousel = ({ data, loading }) => {
-  return <div></div>;
+  const carousalContainer = useRef();
+  const { url } = useSelector((state) => state.home);
+  const navigate = useNavigate();
+
+  return (
+    <div className="carousel">
+      <ContentWrapper>
+        <BsFillArrowLeftCircleFill
+          className="carouselLeftNav arrow"
+          onClick={() => navigation("left")}
+        />
+        <BsFillArrowRightCircleFill
+          className="carouselRightNav arrow"
+          onClick={() => navigation("right")}
+        />
+      </ContentWrapper>
+    </div>
+  );
 };
 
 export default Carousel;

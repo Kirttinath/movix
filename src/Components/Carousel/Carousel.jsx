37ba266a3357aa5835d4dc016a0be9783814c225
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-17 23:29:55 +0530

    Update font and fix typo in release date formatting in carousel component

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -42,7 +42,7 @@ const Carousel = ({ data, loading }) => {
                   <div className="textBlock">
                     <span className="title">{item.title || item.name}</span>
                     <span className="date">
-                      {dayjs(item.release_Date).format("MMM D, YYYY")}
+                      {dayjs(item.release_date).format("MMM D, YYYY")}
                     </span>
                   </div>
                 </div>

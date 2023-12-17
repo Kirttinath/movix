06c5b1e17fbe996a017adc2ac18510f0933c6ee5
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-17 15:26:41 +0530

    Add SwitchTabs component to Trending page

---
+++ src/Components/SwitchTabs/SwitchTabs.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./SwitchTabs.scss";
+const SwitchTabs = () => {
+  return <div></div>;
+};
+
+export default SwitchTabs;
--- src/Pages/Home/Trending/Trending.jsx
+++ src/Pages/Home/Trending/Trending.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ContentWrapper from "../../../Components/ContentWrapper/ContentWrapper";
+import SwitchTabs from "../../../Components/SwitchTabs/SwitchTabs";
 
 const Trending = () => {
+  const onTabChange = (tab) => {};
+
   return (
     <div className="carouselSection">
       <ContentWrapper>
         <span className="carouselTitle">Trending</span>
+        <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
       </ContentWrapper>
     </div>
   );

91effddbacdd3b184f16194758398a6258b64506
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-18 00:42:58 +0530

    Update Genres component to use Redux state and display only the first two genres.

--- src/Components/Genres/Genres.jsx
+++ src/Components/Genres/Genres.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import { useSelector } from "react-redux/es/hooks/useSelector";
 import "./Genres.scss";
 
-const Genres = () => {
-  return <div></div>;
+const Genres = ({ data }) => {
+  const { genres } = useSelector((state) => state.home);
+
+  return (
+    <div className="genres">
+      {data?.map((g) => {
+        if (!genres[g]?.name) return;
+        return (
+          <div key={g} className="genre">
+            {genres[g]?.name}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Genres;

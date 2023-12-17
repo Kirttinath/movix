20c748ce7bc5bf8aa620c9717ee1e5ed1340aa28
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-17 15:35:21 +0530

    Add SwitchTabs component to display tabs

--- src/Components/SwitchTabs/SwitchTabs.jsx
+++ src/Components/SwitchTabs/SwitchTabs.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import "./SwitchTabs.scss";
 const SwitchTabs = ({ data, onTabChange }) => {
-  return <div className="switchingTabs"></div>;
+  return (
+    <div className="switchingTabs">
+      <div className="tabItems">
+        {data.map((tab, index) => (
+          <span key={index} className={`tabItem`}>
+            {tab}
+          </span>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default SwitchTabs;

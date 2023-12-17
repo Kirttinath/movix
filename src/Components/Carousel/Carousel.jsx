0bfe0a91c8b858f7d98db680f759f4530b7da962
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-17 16:08:44 +0530

    Add carousel navigation buttons and lazy-loading for images.

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -1,4 +1,14 @@
-import React from "react";
+import React, { useRef } from "react";
+import {
+  BsFillArrowLeftCircleFill,
+  BsFillArrowRightCircleFill,
+} from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import dayjs from "dayjs";
+import ContentWrapper from "../ContentWrapper/ContentWrapper";
+import Img from "../lazyLoadImage/Img";
+import PosterFallback from "../../assets/no-poster.png";
 import "./Carousel.scss";
 const Carousel = () => {
   return <div></div>;

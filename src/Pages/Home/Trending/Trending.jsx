a2ac7c425188d93f054db8b5e53f1d1ef2758638
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-17 15:50:45 +0530

    Add state and hook for trending endpoint

--- src/Pages/Home/Trending/Trending.jsx
+++ src/Pages/Home/Trending/Trending.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import ContentWrapper from "../../../Components/ContentWrapper/ContentWrapper";
 import SwitchTabs from "../../../Components/SwitchTabs/SwitchTabs";
+import useFetch from "../../../Hooks/useFetch";
 
 const Trending = () => {
+  const [endpoint, setEndpoint] = useState("day");
+
   const onTabChange = (tab) => {};
 
   return (

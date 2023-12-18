bdf919da30d433dbb9ffe73226ef3beeff9252a1
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-18 16:21:01 +0530

    video section added

---
+++ src/Pages/Details/VideosSection/VideosSection.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import ContentWrapper from "../../../Components/ContentWrapper/ContentWrapper";
+import { PlayIcon } from "../PlayIcon";
+import VideoPopup from "../../../Components/VideoPopup/VideoPopup";
+import Img from "../../../Components/LazyLoadImage/Img";
+import "./Videossection.scss";
+const VideosSection = () => {
+  return <div></div>;
+};
+
+export default VideosSection;

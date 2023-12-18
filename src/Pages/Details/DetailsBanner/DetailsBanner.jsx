e170cb1ba1959ce4dc63e317c90d1b6b9c74f830
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-18 11:33:45 +0530

    Add React hooks and components for fetching and displaying movie details.

--- src/Pages/Details/DetailsBanner/DetailsBanner.jsx
+++ src/Pages/Details/DetailsBanner/DetailsBanner.jsx
@@ -1,5 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import dayjs from "dayjs";
+import ContentWrapper from "../../../Components/ContentWrapper/ContentWrapper";
+import useFetch from "../../../Hooks/useFetch";
+import Genres from "../../../Components/Genres/Genres";
+import CircleRating from "../../../Components/CircleRating/CircleRating";
+import Img from "../../../Components/LazyLoadImage/Img";
+import PosterFallback from "../../../assets/no-poster.png";
+
 import "./DetailsBanner.scss";
+
 const DetailsBanner = () => {
   return <div></div>;
 };

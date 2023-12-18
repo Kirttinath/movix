849edd5ebf7cb8f40bf27ee32807e9e58065083a
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-18 22:20:46 +0530

    Changed the import of the store from `./store/store.js` to `./Store/Store.js`.

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import homeSlice from "./homeSlice";
 
-export const store = configureStore({
+export const Store = configureStore({
   reducer: {
     home: homeSlice,
   },

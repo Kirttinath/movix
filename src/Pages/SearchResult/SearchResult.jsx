632e800ea18c00a7f70fcb9137faeda38b0316a6
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-18 18:47:41 +0530

    Update SearchResult page with UI components and styles

--- src/Pages/SearchResult/SearchResult.jsx
+++ src/Pages/SearchResult/SearchResult.jsx
@@ -4,13 +4,18 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import React from "react";
 import { fetchDataFromApi } from "../../utils/api";
 import ContentWrapper from "../../Components/contentWrapper/ContentWrapper";
-import MovieCard from "../../Components/MovieCard/MovieCard";
-import Spinner from "../../Components/Spinner/Spinner";
+// import MovieCard from "../../Components/MovieCard/MovieCard";
+// import Spinner from "../../Components/Spinner/Spinner";
 import noResults from "../../assets/no-results.png";
 import "./SearchResult.scss";
 
 const SearchResult = () => {
-  return <div>SearchResult</div>;
+  const [data, setData] = useState(null);
+  const [pageNum, setPageNum] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const { query } = useParams();
+
+  return <div className="searchResultsPage"></div>;
 };
 
 export default SearchResult;

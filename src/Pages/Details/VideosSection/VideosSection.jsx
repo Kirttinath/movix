b4074512d589aec22271cd39617abbc717b87b66
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-18 17:00:28 +0530

    Add video title and play icon to the videos section

--- src/Pages/Details/VideosSection/VideosSection.jsx
+++ src/Pages/Details/VideosSection/VideosSection.jsx
@@ -36,7 +36,9 @@ const VideosSection = ({ data, loading }) => {
                   <Img
                     src={`https://img.youtube.com/vi/${video.key}/mqdefault.jpg`}
                   />
+                  <PlayIcon />
                 </div>
+                <div className="videoTitle">{video.name}</div>
               </div>
             ))}
           </div>

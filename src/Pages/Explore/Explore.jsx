e4f9cc66bc62437fcc49c820a916f6dfb95c6046
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-18 20:43:57 +0530

    Add Explore page styles and sortby dropdown

--- src/Pages/Explore/Explore.jsx
+++ src/Pages/Explore/Explore.jsx
@@ -2,13 +2,14 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import InfiniteScroll from "react-infinite-scroll-component";
 import Select from "react-select";
+
+import "./Explore.scss";
+
 import useFetch from "../../Hooks/useFetch";
 import { fetchDataFromApi } from "../../utils/api";
 import ContentWrapper from "../../Components/ContentWrapper/ContentWrapper";
 import MovieCard from "../../Components/MovieCard/MovieCard";
 import Spinner from "../../Components/Spinner/Spinner";
-
-import "./Explore.scss";
 
 let filters = {};
 
@@ -24,6 +25,7 @@ const sortbyData = [
   { value: "primary_release_date.asc", label: "Release Date Ascending" },
   { value: "original_title.asc", label: "Title (A-Z)" },
 ];
+
 const Explore = () => {
   const [data, setData] = useState(null);
   const [pageNum, setPageNum] = useState(1);
@@ -58,6 +60,7 @@ const Explore = () => {
       }
     );
   };
+
   useEffect(() => {
     filters = {};
     setData(null);

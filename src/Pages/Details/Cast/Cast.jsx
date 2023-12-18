3aa0421298b107bcb9e45568c3ea8912e04a47ae
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-18 15:36:54 +0530

    Add skeleton loading state for cast page.

--- src/Pages/Details/Cast/Cast.jsx
+++ src/Pages/Details/Cast/Cast.jsx
@@ -4,7 +4,18 @@ import ContentWrapper from "../../../Components/ContentWrapper/ContentWrapper";
 import Img from "../../../Components/LazyLoadImage/Img";
 import avatar from "../../../assets/avatar.png";
 import "./Cast.scss";
-const Cast = () => {
+const Cast = ({ data, loading }) => {
+  const { url } = useSelector((state) => state.home);
+
+  const skeleton = () => {
+    return (
+      <div className="skItem">
+        <div className="circle skeleton"></div>
+        <div className="row skeleton"></div>
+        <div className="row2 skeleton"></div>
+      </div>
+    );
+  };
   return <div></div>;
 };
 

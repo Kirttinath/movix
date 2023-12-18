a2f74a8aeca7d90a3c97ff4e732ae83c0de2e6e1
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-18 15:36:18 +0530

    Importing components in cast components

---
+++ src/Pages/Details/Cast/Cast.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import ContentWrapper from "../../../Components/ContentWrapper/ContentWrapper";
+import Img from "../../../Components/LazyLoadImage/Img";
+import avatar from "../../../assets/avatar.png";
+import "./Cast.scss";
+const Cast = () => {
+  return <div></div>;
+};
+
+export default Cast;

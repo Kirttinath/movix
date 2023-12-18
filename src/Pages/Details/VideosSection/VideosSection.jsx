5e1be62fb0e3c3609ca7ee7a9d6d8b4be76f77cc
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-18 16:22:07 +0530

    Add video popup component and loading skeleton for videos section

--- src/Pages/Details/VideosSection/VideosSection.jsx
+++ src/Pages/Details/VideosSection/VideosSection.jsx
@@ -4,7 +4,19 @@ import { PlayIcon } from "../PlayIcon";
 import VideoPopup from "../../../Components/VideoPopup/VideoPopup";
 import Img from "../../../Components/LazyLoadImage/Img";
 import "./Videossection.scss";
-const VideosSection = () => {
+const VideosSection = ({ data, loading }) => {
+  const [show, setShow] = useState(false);
+  const [videoId, setVideoId] = useState(null);
+
+  const loadingSkeleton = () => {
+    return (
+      <div className="skItem">
+        <div className="thumb skeleton"></div>
+        <div className="row skeleton"></div>
+        <div className="row2 skeleton"></div>
+      </div>
+    );
+  };
   return <div></div>;
 };
 

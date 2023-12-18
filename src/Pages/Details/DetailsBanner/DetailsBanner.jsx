f3ecdd37e4008819b534ee4864c3d42d70b290da
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-18 15:10:03 +0530

    Add writer and creator information to the details banner.

--- src/Pages/Details/DetailsBanner/DetailsBanner.jsx
+++ src/Pages/Details/DetailsBanner/DetailsBanner.jsx
@@ -99,8 +99,39 @@ const DetailsBanner = ({ video, crew }) => {
                       <div className="info">
                         <span className="text bold">Director : </span>
                         <span className="text">
-                          {director.map((d, i) => (
-                            <span key={i}>{d.name}</span>
+                          {director?.map((d, i) => (
+                            <span key={i}>
+                              {d.name}
+                              {director.length - 1 !== i && ", "}
+                            </span>
+                          ))}
+                        </span>
+                      </div>
+                    )}
+
+                    {writer?.length > 0 && (
+                      <div className="info">
+                        <span className="text bold">Writer: </span>
+                        <span className="text">
+                          {writer?.map((d, i) => (
+                            <span key={i}>
+                              {d.name}
+                              {writer.length - 1 !== i && ", "}
+                            </span>
+                          ))}
+                        </span>
+                      </div>
+                    )}
+
+                    {data?.created_by?.length > 0 && (
+                      <div className="info">
+                        <span className="text bold">Creator: </span>
+                        <span className="text">
+                          {data?.created_by?.map((d, i) => (
+                            <span key={i}>
+                              {d.name}
+                              {data?.created_by.length - 1 !== i && ", "}
+                            </span>
                           ))}
                         </span>
                       </div>

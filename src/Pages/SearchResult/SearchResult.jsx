539cf72187e13a19670ef5bead3be968d1cebdb1
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-18 18:33:57 +0530

    Add infinite scroll functionality to search result page

--- src/Pages/SearchResult/SearchResult.jsx
+++ src/Pages/SearchResult/SearchResult.jsx
@@ -1,5 +1,14 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import InfiniteScroll from "react-infinite-scroll-component";
 import React from "react";
+import { fetchDataFromApi } from "../../utils/api";
+import ContentWrapper from "../../Components/contentWrapper/ContentWrapper";
+import MovieCard from "../../Components/MovieCard/MovieCard";
+import Spinner from "../../Components/Spinner/Spinner";
+import noResults from "../../assets/no-results.png";
 import "./SearchResult.scss";
+
 const SearchResult = () => {
   return <div>SearchResult</div>;
 };

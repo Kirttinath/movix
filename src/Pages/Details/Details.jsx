8f9093b41d0f09bf67475f5f1fd171a8e02b7208
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-18 11:26:38 +0530

    Added useFetch hook and useParams hook to fetch data for a specific media type and id.

--- src/Pages/Details/Details.jsx
+++ src/Pages/Details/Details.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import "./Details.scss";
+import useFetch from "../../Hooks/useFetch";
+import { useParams } from "react-router-dom";
+
 const Details = () => {
+  // const { mediaType, id } = useParams();
+  // const [data, loading] = useFetch(`/${mediaType}/${id}`);
   return <div></div>;
 };
 
